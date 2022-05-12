8023f186566ca39857d8b4354ff08e38b1c198f1
KhitraNazariy <khitranazariy19@gmail.com>
2022-05-12 11:48:24 +0300

    hw1

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {IComment} from "../models/IComment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private http: HttpClient) { }
+
+  getComments(): Observable<IComment[]> {
+    return this.http.get<IComment[]>('https://jsonplaceholder.typicode.com/comments')
+  }
+
+}
